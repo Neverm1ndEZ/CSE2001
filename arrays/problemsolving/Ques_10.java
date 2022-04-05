08a6e07e7e69bb13a211d2c0b93b6381fab44717
Neverm1ndEZ <mihirsuman0131@gmail.com>
2022-04-05 21:59:19 +0530

    initail commmit, noob approach.Doesnt pass all test cases

--- arrays/problemsolving/Ques_10.java
+++ arrays/problemsolving/Ques_10.java
@@ -2,14 +2,17 @@ package arrays.problemsolving;
 
 public class Ques_10 {
     public static void main(String[] args) {
-        int[] arr= {7,6,3,5,8};
+        int[] arr = {1, 6, 3, 2};
         System.out.println(SequenceCheck(arr));
     }
 
     public static boolean SequenceCheck(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == 1 && arr[i] == 2 || arr[i] == 3) {
-                return true;
+            if (arr[i] == 1) {
+                i++;
+                if (arr[i] == 2 || arr[i+1] == 3 && arr[i] == 3 || arr[i+1] == 2) {
+                    return true;
+                }
             }
         }
         return false;
