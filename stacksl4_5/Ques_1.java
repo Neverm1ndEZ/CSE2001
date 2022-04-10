dd7cf8766a2366fae13ec73a80b5169aa80a9731
Neverm1ndEZ <mihirsuman0131@gmail.com>
2022-04-10 16:41:00 +0530

    initial commmit

--- stacksl4_5/Ques_1.java
+++ stacksl4_5/Ques_1.java
@@ -2,7 +2,7 @@ package stacksl4_5;
 
 import java.util.Scanner;
 
-public class Ques_2 {
+public class Ques_1 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
